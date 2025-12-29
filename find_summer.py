f7d41833c980af19c0f7fe9734bcc32291132945
Andrew <you@example.com>
2025-12-29 12:07:23 -0500

    move sensitive args to config file

--- find_summer.py
+++ find_summer.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 from geopy.distance import great_circle
 import argparse
+import os
 
 # This code uses Open-Meteo's weather forecasting API to determine which campgrounds near
 # home are likely to have summer-like weekend temperatures within the next few weeks
@@ -10,19 +11,35 @@ import argparse
 # Set up argument parsing
 parser = argparse.ArgumentParser(description="Find upcoming summer-like weekends at campgrounds near home.")
 parser.add_argument('--input_file', type=str, default='all-campgrounds.json', help='Input file containing the list of campgrounds.')
-parser.add_argument('--phone', type=str, help='Phone number to send the top result to (via SMS).')
-parser.add_argument('--home_lat', type=float, default=38.9295911, help='Latitude of home location.')
-parser.add_argument('--home_long', type=float, default=-77.3668801, help='Longitude of home location.')
+parser.add_argument('--config_file', type=str, default='config.json', help='Config file containing phone, home_lat, and home_long values.')
 parser.add_argument('--max_miles', type=float, default=400, help='Maximum distance from home in miles.')
 parser.add_argument('--min_high_temp', type=float, default=70, help='Minimum high temperature for a nice summer day (Fahrenheit).')
 parser.add_argument('--max_high_temp', type=float, default=88, help='Maximum high temperature for a nice summer day (Fahrenheit).')
-
 args = parser.parse_args()
 
-HOME = (args.home_lat, args.home_long)
 MAX_MILES = args.max_miles
 MIN_HIGH_TEMP = args.min_high_temp
 MAX_HIGH_TEMP = args.max_high_temp
+
+config = {}
+if os.path.exists(args.config_file):
+    with open(args.config_file, 'rt') as f:
+        config = json.load(f)
+home_lat = config.get("home_lat", None)
+home_long = config.get("home_long", None)
+phone = config.get("phone", None)
+
+if home_lat and home_long:
+    home = (home_lat, home_long)
+    print(f"Considering campgrounds within {MAX_MILES} miles of home {home}.")
+else:
+    home = None
+    print("Warning: No home location provided. All campgrounds will be considered.")
+
+if phone:
+    print(f"The best option, if found, will be sent via SMS to {phone}.")
+else:
+    print("No phone number provided.  SMS message will not be sent.")
 
 def get_day_of_week(date_str):
     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
@@ -39,7 +56,7 @@ for campground in campgrounds:
     print(f"Checking {name}")
     lat, long = campground["location"].split(",")
     point = (float(lat), float(long))
-    dist = great_circle(HOME, point).miles
+    dist = great_circle(home, point).miles if home else 0
     if dist >= MAX_MILES:
         continue
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&daily=temperature_2m_max&timezone=auto&forecast_days=16&temperature_unit=fahrenheit"
@@ -57,8 +74,6 @@ for campground in campgrounds:
         if temp and temp >= MIN_HIGH_TEMP and temp <= MAX_HIGH_TEMP:
             date = data["daily"]["time"][index]
             day = get_day_of_week(date)
-            point = (float(lat), float(long))
-            dist = great_circle(HOME, point).miles
             if day in ["Saturday","Sunday"]:
                 summer_day = {
                     "dist": dist,
@@ -73,9 +88,9 @@ if summer_days:
     sorted_summer_days = sorted(summer_days, key=lambda d: d['dist'])
     with open("sorted_summer_days.json", "wt") as f:
         f.write(json.dumps(sorted_summer_days, indent=2))
-    if args.phone:
+    if phone:
         resp = requests.post('https://textbelt.com/text', {
-            'phone': args.phone,
+            'phone': phone,
             'message': json.dumps(sorted_summer_days[0]),
             'key': 'textbelt',
         })
